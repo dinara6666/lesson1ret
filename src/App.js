527eb0fb3b39e2b7c887ec901fb2d916c802dc32
Dinara <dinara.kspanovr@gmail.com>
2024-11-09 20:56:14 +0600

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
-
+// src/App.js
+import React from 'react';
+import './App.css'; 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+      <div className="container mt-4">
+        <h3>Пример кнопок Bootstrap 5</h3>
+        
+        <button type="button" className="btn btn-outline-primary m-2">
+          Primary
+        </button>
+        <button type="button" className="btn btn-outline-secondary m-2">
+          Secondary
+        </button>
+        <button type="button" className="btn btn-outline-success m-2">
+          Success
+        </button>
+        <button type="button" className="btn btn-outline-danger m-2">
+          Danger
+        </button>
+        <button type="button" className="btn btn-outline-warning m-2">
+          Warning
+        </button>
+        <button type="button" className="btn btn-outline-info m-2">
+          Info
+        </button>
+        <button type="button" className="btn btn-outline-light m-2">
+          Light
+        </button>
+        <button type="button" className="btn btn-outline-dark m-2">
+          Dark
+        </button>
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+
+
